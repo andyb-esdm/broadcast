21fc21d36d0a2539770e2f18ab3992e8021720d9
andyb-esdm <andybrewer@esdm.co.uk>
2024-01-12 16:33:46 +0000

    better service implementation

--- src/app/linked-records/linked-records.component.ts
+++ src/app/linked-records/linked-records.component.ts
@@ -1,20 +1,19 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecordService } from '../record.service';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, Subscription, switchMap } from 'rxjs';
 import { ILinkedRecord } from '../linked-record.model';
-
-import { broadCastChannel } from '../record.service';
 
 @Component({
   selector: 'app-linked-records',
   templateUrl: './linked-records.component.html',
   styleUrls: ['./linked-records.component.css']
 })
-export class LinkedRecordsComponent {
+export class LinkedRecordsComponent implements OnDestroy {
+  private subscription = new Subscription();
   linkedRecords$: Observable<ILinkedRecord[]> | undefined;
   recordId: number | undefined;
-  constructor(private route: ActivatedRoute, private recordService: RecordService, private cdRef: ChangeDetectorRef) {
+  constructor(private route: ActivatedRoute, private recordService: RecordService, private router: Router) {
     this.linkedRecords$ = this.route.paramMap.pipe(
       switchMap(params => {
         this.recordId = Number(params.get('recordId'));
@@ -22,11 +21,16 @@ export class LinkedRecordsComponent {
       })
     );
 
-    broadCastChannel.onmessage = (event) => {
-      this.recordId = Number(event.data);
-      this.linkedRecords$ = this.recordService.getLinkedRecords(this.recordId);
-      cdRef.detectChanges();
-    }
+    this.subscription.add(this.subscribeToMessages());
+  }
 
+  private subscribeToMessages(): Subscription {
+    return this.recordService.messages$.subscribe(recordId => {
+      this.router.navigate(['linked-records', recordId])
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/record.service.ts
+++ src/app/record.service.ts
@@ -1,15 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { IRecord } from './record.model';
 import { ILinkedRecord } from './linked-record.model';
-import { Observable, of } from 'rxjs';
-export const broadCastChannel = new BroadcastChannel('record-id');
+import { Observable, Subject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RecordService {
-
-
+export class RecordService implements OnDestroy {
+  private broadcastChannel = new BroadcastChannel('record-id');
   private records: IRecord[] = [
     { id: 1, name: 'record1', description: 'description of record 1' },
     { id: 2, name: 'record2', description: 'description of record 2' }
@@ -21,16 +19,25 @@ export class RecordService {
     { id: 3, recordId: 2, name: 'linked to record 2 - 1' },
     { id: 4, recordId: 2, name: 'linked to record 2 - 2' },
     { id: 5, recordId: 2, name: 'linked to record 2 - 2' },
-  ]
+  ];
 
-  constructor() { }
+  private messagesSubject = new Subject<number>();
+  readonly messages$ = this.messagesSubject.asObservable();
+
+  constructor() {
+    this.broadcastChannel.onmessage = (event) => this.messagesSubject.next(Number(event.data));
+  }
 
   getRecord(id: number): Observable<IRecord | undefined> {
-    broadCastChannel.postMessage(id);
+    this.broadcastChannel.postMessage(id);
     return of(this.records.find(record => record.id === id));
   }
 
   getLinkedRecords(recordId: number): Observable<ILinkedRecord[]> {
     return of(this.linkedRecords.filter(linkedRecord => linkedRecord.recordId === recordId));
+  }
+
+  ngOnDestroy(): void {
+    this.broadcastChannel.close();
   }
 }
